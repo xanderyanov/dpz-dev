fddd4eb9c867375b6f69e4ad5c71b25d8abd73f6
xanderyanov <xanderyanov@gmail.com>
2020-02-18 23:58:35 +0400

    18/02/2020 23-58

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,190 +1,194 @@
-$(function () {
+$(function() {
+  $(".menuButton").click(function() {
+    $(this).toggleClass("open");
+    $(".adaptiveMenu__area").slideToggle();
+  });
 
-
+  $(".adaptiveMenuBtnClose").click(function() {
+    $(".menuButton").toggleClass("open");
+    $(".adaptiveMenu__area").slideToggle();
+  });
 });
 
-
 var isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+  }
 };
 
 if ($(".map__area").length) {
-    ymaps.ready(init);
+  ymaps.ready(init);
 
-    function init () {
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [51.53686035750409, 46.02260713621521],
+      zoom: 16,
+      controls: ["zoomControl"]
+    });
 
-        var myMap = new ymaps.Map("map", {
-            center: [51.53686035750409,46.02260713621521],
-            zoom: 16,
-            controls: ['zoomControl']
-        });
-
-        myMap.behaviors.disable('scrollZoom');
-        if (isMobile.any()) {
-            myMap.behaviors.disable('drag');
-        }
-
-
-
-        var myGeoObjects = [];
-
-        myGeoObjects[0] = new ymaps.Placemark([51.53686035750409,46.02260713621521],{
-            clusterCaption: 'Интернет-магазин',
-            hintContent: 'ИНтернет-магазин в Энгельсе!',
-            balloonContentBody: 'г. Энгельс, ул. Тельмана 1'
-        },{
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            iconImageHref: 'assets/img/marker.png',
-            // Размеры метки.
-            iconImageSize: [30, 48],
-            // Смещение левого верхнего угла иконки относительно
-            // её «ножки» (точки привязки).
-            iconImageOffset: [-15, -48]
-        });
-
-        // myGeoObjects[1] = new ymaps.Placemark([51.551021850477284,46.01745698280331],{
-        //     clusterCaption: 'Саратовремеонт',
-        //     hintContent: 'Саратовремонт!',
-        //     balloonContentBody: 'Саратовремонт, Саратов, Танкистов ул., 37'
-        // },{
-        //     iconLayout: 'default#image',
-        //     iconImageHref: 'assets/img/marker.png',
-        //     iconImageSize: [30, 48],
-        //     iconImageOffset: [-15, -48]
-        // });
-
-        var clusterIcons=[{
-            href:'/images/pointer.png',
-            size:[31,40],
-            offset:[0,0]
-        }];
-
-        var clusterer = new ymaps.Clusterer({
-            clusterDisableClickZoom: false,
-            clusterOpenBalloonOnClick: false,
-            // Устанавливаем стандартный макет балуна кластера "Карусель".
-            clusterBalloonContentLayout: 'cluster#balloonCarousel',
-            // Устанавливаем собственный макет.
-            //clusterBalloonItemContentLayout: customItemContentLayout,
-            // Устанавливаем режим открытия балуна.
-            // В данном примере балун никогда не будет открываться в режиме панели.
-            clusterBalloonPanelMaxMapArea: 0,
-            // Устанавливаем размеры макета контента балуна (в пикселях).
-            clusterBalloonContentLayoutWidth: 300,
-            clusterBalloonContentLayoutHeight: 200,
-            // Устанавливаем максимальное количество элементов в нижней панели на одной странице
-            clusterBalloonPagerSize: 5,
-            // Настройка внешего вида нижней панели.
-            // Режим marker рекомендуется использовать с небольшим количеством элементов.
-            clusterBalloonPagerType: 'marker',
-            // Можно отключить зацикливание списка при навигации при помощи боковых стрелок.
-            // clusterBalloonCycling: false,
-            // Можно отключить отображение меню навигации.
-            // clusterBalloonPagerVisible: false
-        });
-
-        clusterer.add(myGeoObjects);
-        myMap.geoObjects.add(clusterer);
+    myMap.behaviors.disable("scrollZoom");
+    if (isMobile.any()) {
+      myMap.behaviors.disable("drag");
     }
 
-}
+    var myGeoObjects = [];
 
+    myGeoObjects[0] = new ymaps.Placemark(
+      [51.53686035750409, 46.02260713621521],
+      {
+        clusterCaption: "Интернет-магазин",
+        hintContent: "ИНтернет-магазин в Энгельсе!",
+        balloonContentBody: "г. Энгельс, ул. Тельмана 1"
+      },
+      {
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#image",
+        iconImageHref: "assets/img/marker.png",
+        // Размеры метки.
+        iconImageSize: [30, 48],
+        // Смещение левого верхнего угла иконки относительно
+        // её «ножки» (точки привязки).
+        iconImageOffset: [-15, -48]
+      }
+    );
+
+    // myGeoObjects[1] = new ymaps.Placemark([51.551021850477284,46.01745698280331],{
+    //     clusterCaption: 'Саратовремеонт',
+    //     hintContent: 'Саратовремонт!',
+    //     balloonContentBody: 'Саратовремонт, Саратов, Танкистов ул., 37'
+    // },{
+    //     iconLayout: 'default#image',
+    //     iconImageHref: 'assets/img/marker.png',
+    //     iconImageSize: [30, 48],
+    //     iconImageOffset: [-15, -48]
+    // });
+
+    var clusterIcons = [
+      {
+        href: "/images/pointer.png",
+        size: [31, 40],
+        offset: [0, 0]
+      }
+    ];
+
+    var clusterer = new ymaps.Clusterer({
+      clusterDisableClickZoom: false,
+      clusterOpenBalloonOnClick: false,
+      // Устанавливаем стандартный макет балуна кластера "Карусель".
+      clusterBalloonContentLayout: "cluster#balloonCarousel",
+      // Устанавливаем собственный макет.
+      //clusterBalloonItemContentLayout: customItemContentLayout,
+      // Устанавливаем режим открытия балуна.
+      // В данном примере балун никогда не будет открываться в режиме панели.
+      clusterBalloonPanelMaxMapArea: 0,
+      // Устанавливаем размеры макета контента балуна (в пикселях).
+      clusterBalloonContentLayoutWidth: 300,
+      clusterBalloonContentLayoutHeight: 200,
+      // Устанавливаем максимальное количество элементов в нижней панели на одной странице
+      clusterBalloonPagerSize: 5,
+      // Настройка внешего вида нижней панели.
+      // Режим marker рекомендуется использовать с небольшим количеством элементов.
+      clusterBalloonPagerType: "marker"
+      // Можно отключить зацикливание списка при навигации при помощи боковых стрелок.
+      // clusterBalloonCycling: false,
+      // Можно отключить отображение меню навигации.
+      // clusterBalloonPagerVisible: false
+    });
+
+    clusterer.add(myGeoObjects);
+    myMap.geoObjects.add(clusterer);
+  }
+}
 
 //################ likeBlock
 
-var share_url = 'https://site-a.ru/';
+var share_url = "https://site-a.ru/";
 var share_title = 'Веб-студия "Сайт-А" - создание и продвижение сайтов, настройка рекламы в ВК';
-var share_desc = 'Главная';
-var share_image = '';
-var share_text = 'Главная';
+var share_desc = "Главная";
+var share_image = "";
+var share_text = "Главная";
 var share_popup_width = 650;
 var share_popup_height = 450;
 
 // var share_links_container = document.getElementById('my_share');
 
+var share_links_container = $(".likeBlock");
 
-var share_links_container = $('.likeBlock');
+if (share_links_container != "NULL") {
+  if (typeof share_popup_width != "number" || typeof share_popup_height != "number") {
+    share_popup_width = 626;
+    share_popup_height = 436;
+  }
 
-if (share_links_container != 'NULL') {
+  share = {
+    twitter: function(purl, ptitle) {
+      url = "http://twitter.com/share?";
+      url += "text=" + encodeURIComponent(ptitle);
+      url += "&url=" + encodeURIComponent(purl);
+      url += "&counturl=" + encodeURIComponent(purl);
+      share.popup(url);
+      return false;
+    },
+    gp: function(purl, ptitle, pimg, text) {
+      url = "https://plus.google.com/share?";
+      url += "url=" + encodeURIComponent(purl);
+      share.popup(url);
+      return false;
+    },
+    mail: function(purl, ptitle, pimg, text) {
+      url = "http://connect.mail.ru/share?";
+      url += "url=" + encodeURIComponent(purl);
+      url += "&title=" + encodeURIComponent(ptitle);
+      url += "&description=" + encodeURIComponent(text);
+      url += "&imageurl=" + encodeURIComponent(pimg);
+      share.popup(url);
+      return false;
+    },
+    vk: function(purl, ptitle, pimg, text) {
+      url = "http://vkontakte.ru/share.php?";
+      url += "url=" + encodeURIComponent(purl);
+      url += "&title=" + encodeURIComponent(ptitle);
+      url += "&description=" + encodeURIComponent(text);
+      url += "&image=" + encodeURIComponent(pimg);
+      url += "&noparse=true";
+      share.popup(url);
+      return false;
+    },
+    ok: function(purl, text) {
+      url = "http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1";
+      url += "&st.comments=" + encodeURIComponent(text);
+      url += "&st._surl=" + encodeURIComponent(purl);
+      share.popup(url);
+      return false;
+    },
+    fb: function(purl, ptitle, pimg, text) {
+      url = "http://www.facebook.com/sharer.php?s=100";
+      url += "&p[title]=" + encodeURIComponent(ptitle);
+      url += "&p[summary]=" + encodeURIComponent(text);
+      url += "&p[url]=" + encodeURIComponent(purl);
+      url += "&p[images][0]=" + encodeURIComponent(pimg);
+      share.popup(url);
+      return false;
+    },
 
-    if (typeof(share_popup_width) != 'number' || typeof(share_popup_height) != 'number') {
-        share_popup_width = 626;
-        share_popup_height = 436;
+    popup: function(url, width, height) {
+      window.open(url, "", "toolbar=0,status=0,width=" + share_popup_width + ",height=" + share_popup_height);
+      return false;
     }
-
-    share = {
-        twitter: function(purl, ptitle) {
-            url  = 'http://twitter.com/share?';
-            url += 'text='      + encodeURIComponent(ptitle);
-            url += '&url='      + encodeURIComponent(purl);
-            url += '&counturl=' + encodeURIComponent(purl);
-            share.popup(url);
-            return false;
-        },
-        gp: function(purl, ptitle, pimg, text) {
-            url  = 'https://plus.google.com/share?';
-            url += 'url='          + encodeURIComponent(purl);
-            share.popup(url);
-            return false;
-        },
-        mail: function(purl, ptitle, pimg, text) {
-            url  = 'http://connect.mail.ru/share?';
-            url += 'url='          + encodeURIComponent(purl);
-            url += '&title='       + encodeURIComponent(ptitle);
-            url += '&description=' + encodeURIComponent(text);
-            url += '&imageurl='    + encodeURIComponent(pimg);
-            share.popup(url);
-            return false;
-        },
-        vk: function(purl, ptitle, pimg, text) {
-            url  = 'http://vkontakte.ru/share.php?';
-            url += 'url='          + encodeURIComponent(purl);
-            url += '&title='       + encodeURIComponent(ptitle);
-            url += '&description=' + encodeURIComponent(text);
-            url += '&image='       + encodeURIComponent(pimg);
-            url += '&noparse=true';
-            share.popup(url);
-            return false;
-        },
-        ok: function(purl, text) {
-            url  = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1';
-            url += '&st.comments=' + encodeURIComponent(text);
-            url += '&st._surl='    + encodeURIComponent(purl);
-            share.popup(url);
-            return false;
-        },
-        fb: function(purl, ptitle, pimg, text) {
-            url  = 'http://www.facebook.com/sharer.php?s=100';
-            url += '&p[title]='     + encodeURIComponent(ptitle);
-            url += '&p[summary]='   + encodeURIComponent(text);
-            url += '&p[url]='       + encodeURIComponent(purl);
-            url += '&p[images][0]=' + encodeURIComponent(pimg);
-            share.popup(url);
-            return false;
-        },
-
-
-        popup: function(url,width,height) {
-            window.open(url,'','toolbar=0,status=0,width='+share_popup_width+',height='+share_popup_height);
-            return false;
-        }
-    };
+  };
 }
