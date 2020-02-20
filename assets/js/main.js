ba953a82e8a2ef71d528d1f69ba10375d8c8e3fe
xanderyanov <xanderyanov@gmail.com>
2020-02-20 21:54:32 +0400

    dpz20-02-20-21-54

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,62 @@ $(function() {
     $(".menuButton").toggleClass("open");
     $(".adaptiveMenu__area").slideToggle();
   });
+
+  $(".vMenu li:has(ul) > a")
+    .addClass("hasInner")
+    .append('<div class="goInner"><i class="icon-next"></i></div>');
+
+  if ($(".swiper-container1").length) {
+    var mySwiper1 = new Swiper(".swiper-container1", {
+      slidesPerView: 1,
+      loop: true,
+      autoplay: {
+        delay: 5500,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination1",
+        type: "bullets",
+        dynamicBullets: true,
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1"
+      },
+      on: {
+        init: function() {
+          console.log("swiper initialized");
+          $(".swiper-slide")
+            .children(".swiper__cadr")
+            .removeClass("animationBaretsky1")
+            .fadeOut(500);
+
+          setTimeout(function() {
+            $(".swiper-slide-active")
+              .children(".swiper__cadr")
+              .fadeIn(500)
+              .addClass("animated")
+              .addClass("animationBaretsky1");
+          }, 500);
+        },
+        slideChange: function() {
+          $(".swiper-slide")
+            .children(".swiper__cadr")
+            .removeClass("animationBaretsky1")
+            .fadeOut(500);
+
+          setTimeout(function() {
+            $(".swiper-slide-active")
+              .children(".swiper__cadr")
+              .fadeIn(500)
+              .addClass("animated")
+              .addClass("animationBaretsky1");
+          }, 500);
+        }
+      }
+    });
+  }
 });
 
 var isMobile = {
